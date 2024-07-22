139cbf834f69b9ae8221a3f3c0f40936c7171ca5
soumyajit126 <127823010+soumyajit126@users.noreply.github.com>
2024-07-22 20:14:07 +0530

    Delete c directory

--- c/printf.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int cwh;
-    scanf("this is the value %i",&cwh);
-    printf("input value %i",&cwh);
-    return 0;
-}
--- c/scanfpro.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int cwh;
-    scanf("this is the value %d",&cwh);
-    printf("input value: cwh=%d",cwh);
-    return 0;
-}
